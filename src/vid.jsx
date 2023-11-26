676d6ea7a991c437425f77c728a9a85c12989673
powerstone666 <imranpasha8225@gmail.com>
2023-11-27 00:47:36 +0530

    v 1.2

--- src/vid.jsx
+++ src/vid.jsx
@@ -2,7 +2,7 @@ import vid from './assets/a.mp4'
 function Vd(){
     return(
         <div className="video"><div>
-            <video controls autoPlay loop muted><source src={vid} style={{cursor:"pointer"}} ></source></video>
+            <video controls  muted><source src={vid} style={{cursor:"pointer"}} ></source></video>
         </div></div>
     );
 }
